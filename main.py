74e1ef291d1892b18a241b7665dfffb63b6109d0
kbaik <>
2025-08-29 00:27:55 -0700

    add report printout to console

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, sort_by_freq
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,10 +9,13 @@ def main():
     book_texts = get_book_text("./books/frankenstein.txt")
     # print(book_texts)
     num_words = get_word_count(book_texts)
-    print(f"{num_words} words found in the document")
-
     char_count = get_char_count(book_texts)
-    print(char_count)
+    sorted_list = sort_by_freq(char_count)
+
+    print(f"""============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------""")
+    for c in sorted_list:
+        print(f"{c["char"]}: {c["freq"]}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -4,17 +4,24 @@ def get_word_count(text):
 
 def get_char_count(texts):
     char_count = {}
-    for text in texts:
-        for char in text:
-            if char.isalpha():
-                c = char.lower()
-                if c in char_count:
-                    char_count[c] += 1
-                else:
-                    char_count[c] = 1
-            else:
-                if char in char_count:
-                    char_count[char] += 1
-                else:
-                    char_count[char] = 1
+    for char in texts:
+        c = char.lower()
+        if c in char_count:
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
     return char_count
+
+def sort_on(items):
+    return items["freq"]
+
+
+def sort_by_freq(char_dict):
+    char_list = []
+    dict_keys = char_dict.keys()
+    for key in dict_keys:
+        if key.isalpha():
+            char_list.append({"char": key, "freq": char_dict[key]})
+    char_list.sort(reverse=True, key=sort_on)
+    
+    return char_list
