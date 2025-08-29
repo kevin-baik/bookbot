1e44075d701dd9b3aa6521bf659d10d54bb52ad3
kbaik <>
2025-08-29 11:50:19 -0700

    handle system arguements as book path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, get_char_count, sort_by_freq
 
 def get_book_text(filepath):
@@ -6,16 +7,23 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    book_texts = get_book_text("./books/frankenstein.txt")
-    # print(book_texts)
+    try:
+        sys.argv[1]
+    except Exception as e:
+        sys.exit("Usage: python3 main.py <path_to_book>")
+    book_path = sys.argv[1]
+    book_texts = get_book_text(book_path)
     num_words = get_word_count(book_texts)
     char_count = get_char_count(book_texts)
     sorted_list = sort_by_freq(char_count)
 
-    print(f"""============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------""")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     for c in sorted_list:
         print(f"{c["char"]}: {c["freq"]}")
     print("============= END ===============")
 
-if __name__ == "__main__":
-    main()
+main()
